322dba5b54fbee687e7de4c27c23c370a5ab8941
Backspe <bschoe@gmail.com>
2015-03-30 20:37:46 +0900

    show articles

--- helloswpp/app/controllers/articles_controller.rb
+++ helloswpp/app/controllers/articles_controller.rb
@@ -1,8 +1,27 @@
 class ArticlesController < ApplicationController
+
+  def index
+    @articles = Article.all
+  end
+  def show
+    @article = Article.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:article].inspect
+    @article = Article.new(article_params)
+# Initialize new model with filtered parameter.
+    @article.save
+
+    redirect_to @article
+
+#    render plain: params[:article].inspect
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
   end
 end
